f35402e1e98d3dca3e202ce800ba72a8354f552f
johndavid400 <johndavid400@yahoo.com>
2012-01-31 16:34:06 -0600

    hooks

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,5 @@
 GithubSuppliers::Application.routes.draw do
 
   root :to => "products#index"
-  gem "spree_suppliers", :path => "vendor/extensions/spree_suppliers", :require => "spree_suppliers"
-
-
-
-
 
 end
--- vendor/extensions/spree_suppliers/config/routes.rb
+++ vendor/extensions/spree_suppliers/config/routes.rb
@@ -3,4 +3,17 @@ Rails.application.routes.draw do
   resources :suppliers
   resources :supplier_invoices
   resources :invoice_items
+
+  namespace :admin do
+    resources :products do
+      resources :suppliers, :member => {:select => :post, :remove => :post}, :collection => {:available => :post, :selected => :get}
+      member do
+        get :publish
+        get :unpublish
+      end
+    end
+    resources :orders do
+      resources :suppliers, :collection => {:line_items => :get}
+    end
+  end
 end
