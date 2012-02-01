ff11e70a2d6fb84afa7f7745088df6658c6cccb8
johndavid400 <johndavid400@yahoo.com>
2012-02-01 17:09:10 -0600

    added suppliers list to products form

--- vendor/extensions/spree_suppliers/lib/spree_suppliers.rb
+++ vendor/extensions/spree_suppliers/lib/spree_suppliers.rb
@@ -14,6 +14,45 @@ module SpreeSuppliers
         belongs_to :supplier
       end
 
+      Admin::OrdersController.class_eval do
+        def show
+          load_order
+          if current_user.has_role?("vendor")
+            @invoices = @order.supplier_invoices
+            @invoices.select! {|s| s.supplier_id == current_user.supplier.id}
+          else
+            @invoices = @order.supplier_invoices
+          end
+          respond_with(@order)
+        end
+
+        def index
+          params[:search] ||= {}
+          params[:search][:completed_at_is_not_null] ||= '1' if Spree::Config[:show_only_complete_orders_by_default]
+          @show_only_completed = params[:search][:completed_at_is_not_null].present?
+          params[:search][:meta_sort] ||= @show_only_completed ? 'completed_at.desc' : 'created_at.desc'
+          @search = Order.metasearch(params[:search])
+          if !params[:search][:created_at_greater_than].blank?
+            params[:search][:created_at_greater_than] = Time.zone.parse(params[:search][:created_at_greater_than]).beginning_of_day rescue ""
+          end
+          if !params[:search][:created_at_less_than].blank?
+            params[:search][:created_at_less_than] = Time.zone.parse(params[:search][:created_at_less_than]).end_of_day rescue ""
+          end
+          if @show_only_completed
+            params[:search][:completed_at_greater_than] = params[:search].delete(:created_at_greater_than)
+            params[:search][:completed_at_less_than] = params[:search].delete(:created_at_less_than)
+          end
+          @orders = Order.metasearch(params[:search]).paginate(
+            :include => [:user, :shipments, :payments],
+            :per_page => Spree::Config[:orders_per_page],
+            :page => params[:page])
+          if current_user.has_role?("vendor")
+            @orders.select! {|o| o.supplier_invoices.select {|s| s.supplier_id == current_user.supplier.id}.size > 0}
+          end
+          respond_with(@orders)
+        end
+      end
+
       Order.class_eval do
         has_many :supplier_invoices
         def generate_invoices(order)
@@ -55,6 +94,75 @@ module SpreeSuppliers
             :name           => "order" ,
             :user_id        => (User.respond_to?(:current) && User.current.try(:id)) || self.user_id
           })
+        end
+      end
+
+      Admin::ProductsController.class_eval do
+        before_filter :load
+        before_filter :load_index, :only => [:index]
+        before_filter :edit_before, :only => [:edit]
+        create.before :create_before
+        create.fails :reset
+        update.before :update_taxons
+
+        def load
+          @suppliers = Supplier.find(:all, :order => "name")
+          @options = Taxon.all
+        end
+
+        def load_index
+          if current_user.roles.member?(Role.find_by_name("vendor"))
+            @collection.select! {|c| c.supplier_id == current_user.supplier.id}
+          end
+        end
+
+        #indicate that we want to create a new product
+        def new
+          @object = Product.new()
+          @status = true
+          @suppliers = Supplier.all
+        end
+
+        def edit_before
+          @suppliers = Supplier.all
+          @status = false
+        end
+
+        def taxon_push object
+          object.taxons = []
+          Taxon.all.map {|m| object.taxons.push(Taxon.find_by_id(params[m.name])) if params.member?(m.name)}
+          return object
+        end
+
+        def reset
+          @status = true
+        end
+
+        def update_taxons
+          @object = taxon_push(@object)
+        end
+
+        def create_before
+          if current_user.has_role?("vendor")
+            @object = current_user.supplier.products.build(params[:product])
+          else
+            @object = Product.new(params[:product])
+          end
+          @object = taxon_push(@object)
+        end
+
+        def publish
+          p = Product.find_by_name(params[:id])
+          p.available_on = Date.today
+          p.save
+          redirect_to edit_admin_product_path(p)
+        end
+
+        def unpublish
+          p = Product.find_by_name(params[:id])
+          p.available_on = nil
+          p.save
+          redirect_to edit_admin_product_path(p)
         end
       end
 
